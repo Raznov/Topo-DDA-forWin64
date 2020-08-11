3e6ca427dd74a7f6d2a64d82cc185a06d6a3f69e
Raznov <yzraznov@gmail.com>
2020-08-10 21:47:04 -0500

    obj z can be any value now

--- Topo-DDA-forWin64/test.cpp
+++ Topo-DDA-forWin64/test.cpp
@@ -19,7 +19,10 @@ int main(){
 
     
     int Nx, Ny, Nz;
-    Nx = 83; Ny = 83; Nz = 12;
+    Nx = 102; Ny = 102; Nz = 15;
+    //Nx = 103;
+    //Ny = 103;
+    //Nz = 16;
     int N = 0;
     VectorXi total_space = build_a_bulk(Nx, Ny, Nz);
     list<Structure> ln;
@@ -28,18 +31,22 @@ int main(){
     Vector3i direction;
     Vector3d l;
     Vector3d center;
-    l << 39.0, 39.0, 11.0;
-    center << 19.5, 19.5, 5.5;
+    l << 49.0, 49.0, 0.0;
+    center << 24.5, 24.5, 14.0;
+    //l << 39.0, 39.0, 0.0;
+    //center << 19.5, 19.5, 11.0;
+    //l << 79.0, 79.0, 11.0;
+    //center << 39.5, 39.5, 5.5;
     direction<<0,0,-1;
-    int times = 10;
+    int times = 15;
     Structure s1(S.get_total_space(), "ONES", l, center, 1);
     Structure s2(S.get_total_space(), &s1, 1);
     Structure s3(S.get_total_space(), &s1, 2);
     Structure s4(S.get_total_space(), &s1, 3);
-    //Structure s5(S.get_total_space(), &s1, direction, 12, 2);
-    //Structure s6(S.get_total_space(), &s2, direction, 12, 2);
-    //Structure s7(S.get_total_space(), &s3, direction, 12, 2);
-    //Structure s8(S.get_total_space(), &s4, direction, 12, 2);
+    Structure s5(S.get_total_space(), &s1, direction, times, 2);
+    Structure s6(S.get_total_space(), &s2, direction, times, 2);
+    Structure s7(S.get_total_space(), &s3, direction, times, 2);
+    Structure s8(S.get_total_space(), &s4, direction, times, 2);
 
 
 
@@ -47,15 +54,15 @@ int main(){
     S = S + s2;
     S = S + s3;
     S = S + s4;
-    //S = S + s5;
-    //S = S + s6;
-    //S = S + s7;
-    //S = S + s8;
+    S = S + s5;
+    S = S + s6;
+    S = S + s7;
+    S = S + s8;
     //S=S+s0;
     //S.show_something_about_Structures();
 
     
-    double d = 25;
+    double d = 20;
 
     double lam = 500;
     Vector3d n_K;
@@ -67,7 +74,7 @@ int main(){
     //Model test_model(&S, d, lam, n_K, E0, n_E0, material);
     double epsilon = 100;
 
-    double focus = 300;   //nm       
+    double focus = 290;   //nm       
 
     //Vector3d r;
     //r<<center(0)*d, center(1)*d, focus;
@@ -75,7 +82,7 @@ int main(){
 
     int MAX_ITERATION_DDA = 10000;
     double MAX_ERROR = 0.00001;
-    int MAX_ITERATION_EVO = 10;
+    int MAX_ITERATION_EVO = 50;
 
     list<string> ObjectFunctionNames{ "ExtSurfaceEExp_CPU" };
 
