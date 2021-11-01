bfdeb826200471739912efb99879dbdc0b8f731f
Raznov <yzraznov@gmail.com>
2021-11-01 04:06:56 -0500

    eigen dependency and a iteration func

--- Topo-DDA-forWin64/test.cpp
+++ Topo-DDA-forWin64/test.cpp
@@ -4,206 +4,89 @@
 
 int main() {
 
-    ofstream TotalTime;
-    TotalTime.open("TotalTime.txt");
-    high_resolution_clock::time_point t_start = high_resolution_clock::now();
-
-
-
+    string save_position;
+    Vector3i bind; 
     Vector3d l;
-    Vector3d center;
-    //l << 31.0, 31.0, 9.0;
-    l << 20.0, 20.0, 16.0;
-    center << l(0) / 2, l(1) / 2, l(2) / 2;
+    int MAX_ITERATION_EVO = 200;
 
-    int Nx, Ny, Nz;
-    //Nx = 103; Ny = 103; Nz = 16;
-    Nx = round(l(0) + 1); Ny = round(l(1) + 1); Nz = round(l(2) + 1);
-    cout << center << endl;
-    //Nx = 23; Ny = 23; Nz = 10;
-    int N = 0;
-    VectorXi total_space = build_a_bulk(Nx, Ny, Nz);
-    list<Structure> ln;
-    Space S(&total_space, Nx, Ny, Nz, N, &ln);
+    //-------------------------------------------bind2------------------------------------------
+    //thick100
+    save_position = ".\\thick100-diel2d5-phi0theta0-lam500-size1000-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 40.0, 40.0, 4.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
-    Vector3i direction;
+    save_position = ".\\thick100-diel2d5-phi0theta0-lam500-size1500-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 60.0, 60.0, 4.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
-    //l << 20.0, 20.0, 9.0;
-    //center << 10.0, 10.0, 4.5;
-    Structure s1(S.get_total_space(), l, center);
+    save_position = ".\\thick100-diel2d5-phi0theta0-lam500-size2000-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 80.0, 80.0, 4.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
+    save_position = ".\\thick100-diel2d5-phi0theta0-lam500-size2500-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 100.0, 100.0, 4.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
+    //thick200
+    save_position = ".\\thick200-diel2d5-phi0theta0-lam500-size1000-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 40.0, 40.0, 8.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
+    save_position = ".\\thick200-diel2d5-phi0theta0-lam500-size1500-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 60.0, 60.0, 8.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
-    S = S + s1;
-    double d = 25;
+    save_position = ".\\thick200-diel2d5-phi0theta0-lam500-size2000-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 80.0, 80.0, 8.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
-    Vector3i bind(1, 1, 1);
-    //SpacePara spacepara(&S, bind, "ONES", "ZEROS", r);
+    save_position = ".\\thick200-diel2d5-phi0theta0-lam500-size2500-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 100.0, 100.0, 8.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
+    //thick300
+    save_position = ".\\thick300-diel2d5-phi0theta0-lam500-size1000-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 40.0, 40.0, 12.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
-    SpacePara spacepara(&S, bind, "RANDOM");
+    save_position = ".\\thick300-diel2d5-phi0theta0-lam500-size1500-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 60.0, 60.0, 12.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
+    save_position = ".\\thick300-diel2d5-phi0theta0-lam500-size2000-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 80.0, 80.0, 12.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
-    double E0 = 1.0;
+    save_position = ".\\thick300-diel2d5-phi0theta0-lam500-size2500-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 100.0, 100.0, 12.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
+    //thick400
+    save_position = ".\\thick400-diel2d5-phi0theta0-lam500-size1000-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 40.0, 40.0, 16.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
+    save_position = ".\\thick400-diel2d5-phi0theta0-lam500-size1500-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 60.0, 60.0, 16.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
-    double epsilon = 0.2;
-    //double epsilon = 1;
-
-    //double focus = (l(2) + 2) * d;   //nm       
-    double focus = 8 * d;
-    
-    cout << focus << endl;
-
-    int MAX_ITERATION_DDA = 100000;
-    double MAX_ERROR = 0.00001;
-    int MAX_ITERATION_EVO = 50;
-
-    //list<string> ObjectFunctionNames{ "IntegratedE" };
-    list<string> ObjectFunctionNames{ "pointE" };
-
-    double exponent = 2;
-    double ratio = 4;
-
-    list<double> ObjectParameter{ center(0)*d, center(1)*d, focus };
-
-    bool HavePathRecord = false;
-    bool HavePenalty = false;
-    bool HaveOriginHeritage = false;
-    bool HaveAdjointHeritage = false;
-    double PenaltyFactor = 1;
-    list<list<double>*> ObjectParameters{ &ObjectParameter };
-    string save_position = ".\\300nm-SiO2-8-period\\";
-
-    Vector3d n_K;
-    Vector3d n_E0;
-
-
-
-    list<DDAModel> ModelList;
-    list<DDAModel*> ModelpointerList;
-
-    ofstream AngleInfo(save_position + "AngleInfo.txt");
-    ofstream nEInfo(save_position + "nEInfo.txt");
-
-    int theta_num = 1;
-    VectorXd theta(theta_num);
-    theta << 0;
-    int phi_num = 1;
-    VectorXd phi(phi_num);
-    phi << 0;
-    int lam_num = 1;
-    VectorXd lam(lam_num);
-    lam << 300;
-
-    CoreStructure CStr(&spacepara, d);
-    list<AProductCore> CoreList;
-    list<AProductCore*> CorePointList;
-    Vector2cd material;
-    material = Get_2_material("Air", "SiO2", lam(0), "nm");
-    //AProductCore Core1(&CStr, lam(0), material, "LDR");
-
-    ofstream Common;
-    Common.open(save_position + "Commondata.txt");
-    Common << CStr.get_Nx() << endl << CStr.get_Ny() << endl << CStr.get_Nz() << endl << CStr.get_N() << endl;
-    Common << (spacepara.get_geometry()) << endl;
-    Common << d << endl;
-    Common << n_E0 << endl;
-    Common << n_K << endl;
-
-    int m, n;
-    double Lm, Ln;
-    m = 50;
-    n = 50;
-    Lm = 32 * d;
-    Ln = 32 * d;
-    //AProductCore Core1(&CStr, lam(0), material, m, n, Lm, Ln, "FCD");
-    AProductCore Core1(&CStr, lam(0), material, "FCD");
-    //material = Get_2_material("Air", "2.5", lam(1), "nm");
-    //AProductCore Core2(&CStr, lam(1), material, "LDR");
-    //material = Get_2_material("Air", "2.5", lam(2), "nm");
-    //AProductCore Core3(&CStr, lam(2), material, "LDR");
-    CorePointList.push_back(&Core1);
-    //CorePointList.push_back(&Core2);
-    //CorePointList.push_back(&Core3);
-
-    /*
-    for (int k = 0; k <= lam_num - 1; k++) {
-        Vector2cd material = Get_2_material("Air", "SiO2", lam(k), "nm");
-        AProductCore Core_tmp(&CStr, lam(k), material);
-        CoreList.push_back(Core_tmp);
-    }
-    */
-
-    list<AProductCore*>::iterator it = CorePointList.begin();
-    for (int k = 0; k <= lam_num - 1; k++) {
-        AProductCore* Core = (*it);
-        for (int i = 0; i <= theta_num - 1; i++) {
-            for (int j = 0; j <= phi_num - 1; j++) {
-                if (theta(i) != 0) {
-                    double theta_tmp = theta(i) * M_PI / 180;
-                    double phi_tmp = phi(j) * M_PI / 180;
-                    n_K << sin(theta_tmp) * cos(phi_tmp), sin(theta_tmp)* sin(phi_tmp), cos(theta_tmp);
-                    n_E0 = nEPerpinXZ(theta_tmp, phi_tmp);
-                    if (CheckPerp(n_E0, n_K) == false) {
-                        cout << "----------------------------------------theta" << theta[i] << "phi" << phi[j] << "Not perpendicular---------------------------------------" << endl;
-                    }
-                    if (k == 0) {
-                        AngleInfo << theta[i] << endl;
-                        AngleInfo << phi[j] << endl;
-                        nEInfo << n_E0(0) << " " << n_E0(1) << " " << n_E0(2) << endl;
-                    }
-                    DDAModel Model(Core, n_K, E0, n_E0);
-                    ModelList.push_back(Model);
-                }
-            }
-        }
-
-        double theta_tmp = 0 * M_PI / 180;
-        double phi_tmp = 0 * M_PI / 180;
-        n_K << sin(theta_tmp) * cos(phi_tmp), sin(theta_tmp)* sin(phi_tmp), cos(theta_tmp);
-        n_E0 = nEPerpinXZ(theta_tmp, phi_tmp);
-        if (CheckPerp(n_E0, n_K) == false) {
-            cout << "----------------------------------------theta" << 0 << "phi" << 0 << "Not perpendicular---------------------------------------" << endl;
-        }
-        if (k == 0) {
-            AngleInfo << 0.0 << endl;
-            AngleInfo << 0.0 << endl;
-            nEInfo << n_E0(0) << " " << n_E0(1) << " " << n_E0(2) << endl;
-        }
-        DDAModel Model(Core, n_K, E0, n_E0);
-        ModelList.push_back(Model);
-
-        it++;
-    }
+    save_position = ".\\thick400-diel2d5-phi0theta0-lam500-size2500-focus50-bind2\\";
+    bind << 2, 2, 2;
+    l << 100.0, 100.0, 16.0;
+    Evo_single(save_position, bind, l, MAX_ITERATION_EVO);
 
 
-    cout << "nK" << n_K << endl;
-    cout << "nE0" << n_E0 << endl;
-
-    AngleInfo.close();
-    nEInfo.close();
-    cout << "Number of DDA Model : " << ModelList.size() << endl;
-
-    list<DDAModel>::iterator it1 = ModelList.begin();
-    for (int i = 0; i <= ModelList.size() - 1; i++) {
-        ModelpointerList.push_back(&(*it1));
-        it1++;
-    }
-
-
-    EvoDDAModel EModel(&ObjectFunctionNames, &ObjectParameters, epsilon, HavePathRecord, HavePenalty, HaveOriginHeritage, HaveAdjointHeritage, PenaltyFactor, save_position, &CStr, ModelpointerList);
-
-
-    EModel.EvoOptimization(MAX_ITERATION_DDA, MAX_ERROR, MAX_ITERATION_EVO, "Adam");
-
-
-
-
-
-    high_resolution_clock::time_point t_end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(t_end - t_start).count();
-    TotalTime << duration / 1000 << endl;
-    TotalTime.close();
 
     return 0;
 
--- Topo-DDA-forWin64/tools.cpp
+++ Topo-DDA-forWin64/tools.cpp
@@ -90,6 +90,130 @@ void Evo_Focus(SpacePara* spacepara_tmp, CoreStructure* CStr, DDAModel* TestMode
     
 }
 
+void Evo_single(string save_position, Vector3i bind, Vector3d l, int MAX_ITERATION_EVO) {
+    ofstream TotalTime;
+    TotalTime.open(save_position + "TotalTime.txt");
+    high_resolution_clock::time_point t_start = high_resolution_clock::now();
+    Vector3d center;
+    center << l(0) / 2, l(1) / 2, l(2) / 2;
+    int Nx, Ny, Nz;
+    Nx = round(l(0) + 3); Ny = round(l(1) + 3); Nz = round(l(2) + 1);
+    cout << center << endl;
+    int N = 0;
+    VectorXi total_space = build_a_bulk(Nx, Ny, Nz);
+    list<Structure> ln;
+    Space S(&total_space, Nx, Ny, Nz, N, &ln);
+
+    Vector3i direction;
+    Structure s1(S.get_total_space(), l, center);
+    S = S + s1;
+    SpacePara spacepara(&S, bind, "ONES");
+    double d = 25;
+    double E0 = 1.0;
+    double epsilon = 10;
+    double focus = (l(2) + 2) * d;   //nm       
+    //double focus = (l(2) - 6) * d;
+    cout << focus << endl;
+    int MAX_ITERATION_DDA = 100000;
+    double MAX_ERROR = 0.00001;
+    
+    list<string> ObjectFunctionNames{ "PointE" };
+    double exponent = 2;
+    double ratio = 4;
+    list<double> ObjectParameter{ center(0) * d,center(1) * d,focus };
+    bool HavePathRecord = false;
+    bool HavePenalty = false;
+    bool HaveOriginHeritage = true;
+    bool HaveAdjointHeritage = false;
+    double PenaltyFactor = 0.0001;
+    list<list<double>*> ObjectParameters{ &ObjectParameter };
+    Vector3d n_K;
+    Vector3d n_E0;
+    list<DDAModel> ModelList;
+    list<DDAModel*> ModelpointerList;
+    ofstream AngleInfo(save_position + "AngleInfo.txt");
+    ofstream nEInfo(save_position + "nEInfo.txt");
+    int theta_num = 1;
+    VectorXd theta(theta_num);
+    theta << 0;
+    int phi_num = 1;
+    VectorXd phi(phi_num);
+    phi << 0;
+    int lam_num = 1;
+    VectorXd lam(lam_num);
+    lam << 500;
+    CoreStructure CStr(&spacepara, d);
+    list<AProductCore> CoreList;
+    list<AProductCore*> CorePointList;
+    Vector2cd material;
+    material = Get_2_material("Air", "2.5", lam(0), "nm");
+    AProductCore Core1(&CStr, lam(0), material, "LDR");
+    CorePointList.push_back(&Core1);
+    ofstream Common;
+    Common.open(save_position + "Commondata.txt");
+    Common << CStr.get_Nx() << endl << CStr.get_Ny() << endl << CStr.get_Nz() << endl << CStr.get_N() << endl;
+    Common << (spacepara.get_geometry()) << endl;
+    Common << d << endl;
+    Common << n_E0 << endl;
+    Common << n_K << endl;
+    list<AProductCore*>::iterator it = CorePointList.begin();
+    for (int k = 0; k <= lam_num - 1; k++) {
+        AProductCore* Core = (*it);
+        for (int i = 0; i <= theta_num - 1; i++) {
+            for (int j = 0; j <= phi_num - 1; j++) {
+                if (theta(i) != 0) {
+                    double theta_tmp = theta(i) * M_PI / 180;
+                    double phi_tmp = phi(j) * M_PI / 180;
+                    n_K << sin(theta_tmp) * cos(phi_tmp), sin(theta_tmp)* sin(phi_tmp), cos(theta_tmp);
+                    n_E0 = nEPerpinXZ(theta_tmp, phi_tmp);
+                    if (CheckPerp(n_E0, n_K) == false) {
+                        cout << "----------------------------------------theta" << theta[i] << "phi" << phi[j] << "Not perpendicular---------------------------------------" << endl;
+                    }
+                    if (k == 0) {
+                        AngleInfo << theta[i] << endl;
+                        AngleInfo << phi[j] << endl;
+                        nEInfo << n_E0(0) << " " << n_E0(1) << " " << n_E0(2) << endl;
+                    }
+                    DDAModel Model(Core, n_K, E0, n_E0);
+                    ModelList.push_back(Model);
+                }
+            }
+        }
+        double theta_tmp = 0 * M_PI / 180;
+        double phi_tmp = 0 * M_PI / 180;
+        n_K << sin(theta_tmp) * cos(phi_tmp), sin(theta_tmp)* sin(phi_tmp), cos(theta_tmp);
+        n_E0 = nEPerpinXZ(theta_tmp, phi_tmp);
+        if (CheckPerp(n_E0, n_K) == false) {
+            cout << "----------------------------------------theta" << 0 << "phi" << 0 << "Not perpendicular---------------------------------------" << endl;
+        }
+        if (k == 0) {
+            AngleInfo << 0.0 << endl;
+            AngleInfo << 0.0 << endl;
+            nEInfo << n_E0(0) << " " << n_E0(1) << " " << n_E0(2) << endl;
+        }
+        DDAModel Model(Core, n_K, E0, n_E0);
+        ModelList.push_back(Model);
+
+        it++;
+    }
+    AngleInfo.close();
+    nEInfo.close();
+    cout << "Number of DDA Model : " << ModelList.size() << endl;
+    list<DDAModel>::iterator it1 = ModelList.begin();
+    for (int i = 0; i <= ModelList.size() - 1; i++) {
+        ModelpointerList.push_back(&(*it1));
+        it1++;
+    }
+    EvoDDAModel EModel(&ObjectFunctionNames, &ObjectParameters, epsilon, HavePathRecord, HavePenalty, HaveOriginHeritage, HaveAdjointHeritage, PenaltyFactor, save_position, &CStr, ModelpointerList);
+    EModel.EvoOptimization(MAX_ITERATION_DDA, MAX_ERROR, MAX_ITERATION_EVO, "Adam");
+    high_resolution_clock::time_point t_end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(t_end - t_start).count();
+    TotalTime << duration / 1000 << endl;
+    TotalTime.close();
+
+    return;
+}
+
 //Find the Max and Min of the input geometry in each direction
 MatrixXi find_scope_3_dim(VectorXi* x) {
     int N = round((*x).size() / 3);
